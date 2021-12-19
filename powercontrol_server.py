30ac02cc6712afad180a34f891efb0de793fa21d
s4b7r <github@s4b7r.de>
2021-12-19 16:42:18 +0100

    Use cron syntax in timeframes and duration

--- powercontrol_server.py
+++ powercontrol_server.py
@@ -2,7 +2,18 @@ from starlette.applications import Starlette
 from starlette.responses import PlainTextResponse
 import uvicorn
 import json
-from datetime import time, datetime
+from datetime import time, datetime, timedelta, date
+
+
+def starttime_from_cronstring(cronstring):
+    tokens = cronstring.split(' ')
+    minutes = list(map(int, tokens[0].split(',')))
+    hours = list(map(int, tokens[1].split(',')))
+    # CAVEAT: Says it uses cron strings, but actually uses only one hour and minute from cron string. <>
+    hour = hours[0]
+    minute = minutes[0]
+    starttime = f'{hour:02d}:{minute:02d}:00'
+    return time.fromisoformat(starttime)
 
 
 def load_timeframes():
@@ -10,18 +21,30 @@ def load_timeframes():
     with open('timeframes.json', 'r') as timeframes_file:
         timeframes = json.load(timeframes_file)
     for frame in timeframes:
-        frame['start'] = time.fromisoformat(frame['start'])
-        frame['end'] = time.fromisoformat(frame['end'])
+        # CAVEAT: Says it uses cron strings, but can't go with wildcard for day of month. <>
+        frame['days'] = list(map(int, frame['cron-start'].split(' ')[2].split(',')))
+        frame['duration'] = timedelta(minutes=int(frame['duration']))
 
 
-def is_in_frame(time, frame):
-    time = time.time()
-    return time >= frame['start'] and time <= frame['end']
+def is_in_frame(time, frame, day_offset=0):
+    # CAVEAT: Says it uses cron strings, but actually ignores month and day of week. <>
+    start = datetime.combine(date=date.today() - timedelta(days=day_offset), time=starttime_from_cronstring(frame['cron-start']))
+    if start.day not in frame['days']:
+        return False
+    end = start + frame['duration']
+    print(end)
+    return time >= start and time <= end
+
+
+def is_in_frame_of_yesterday(time, frame):
+    return is_in_frame(time, frame, day_offset=1)
 
 
 def is_in_any_frame(time):
     for frame in timeframes:
         if is_in_frame(time, frame):
+            return True
+        if is_in_frame_of_yesterday(time, frame):
             return True
     return False
 
