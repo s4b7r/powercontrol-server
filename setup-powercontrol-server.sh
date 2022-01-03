80dfaf8060ebbc05a1444947a1694297ab580d3b
s4b7r <github@s4b7r.de>
2022-01-03 21:22:09 +0100

    Make run wrapper executable in setup

--- setup-powercontrol-server.sh
+++ setup-powercontrol-server.sh
@@ -7,6 +7,8 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install starlette uvicorn
 
+chmod u+x run-powercontrol-server.sh
+
 ln -s "$(pwd)/powercontrol-server.service" /etc/systemd/system/powercontrol-server.service
 sed -i "s;INSTALL_DIR;$(pwd);g" powercontrol-server.service
 systemctl daemon-reload
