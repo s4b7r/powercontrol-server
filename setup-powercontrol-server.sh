38918c61a0595f18a144e3f64539c4ff5f794f69
s4b7r <github@s4b7r.de>
2022-01-03 20:51:12 +0100

    Fix filling of install dir in service file

--- setup-powercontrol-server.sh
+++ setup-powercontrol-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt update
 apt -y install etherwake
 
 python3 -m venv venv
@@ -7,7 +8,7 @@ source venv/bin/activate
 pip3 install starlette uvicorn
 
 ln -s "$(pwd)/powercontrol-server.service" /etc/systemd/system/powercontrol-server.service
-sed -i 's/INSTALL_DIR/$(pwd)/g' /etc/systemd/system/powercontrol-server.service
+sed -i "s;INSTALL_DIR;$(pwd);g" powercontrol-server.service
 systemctl daemon-reload
 
 echo "Configure your timeframes.json and execute configure-crontab.py."
