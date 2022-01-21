678305980a415300296726000539a56d326eb670
s4b7r <github@s4b7r.de>
2022-01-21 15:58:45 +0100

    Don't use default-mac dict as timeframe for config

--- configure-crontab.py
+++ configure-crontab.py
@@ -7,4 +7,8 @@ with open('timeframes.json', 'r') as timeframes_file:
 pwd = str(Path('.').resolve())
 with open('powercontrol-server-crontab', mode='w') as cronfile:
     for frame in timeframes:
+        if 'default-mac' in frame:
+            DEFAULT_MAC = frame['default-mac']
+            continue
+
         cronfile.write(f'{frame["cron-start"]} root {pwd}/wake.sh {frame["client-mac"]}\n')
